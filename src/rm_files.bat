8becc8140194c6951f3ca116ade866fd4250166c
yogendra-yatnalkar <yogenyat@gmail.com>
2020-07-13 14:30:41 +0530

    .

--- src/rm_files.bat
+++ src/rm_files.bat
@@ -2,43 +2,48 @@
 D:
 cd "Yogendra D\Self_driving_Car_for_GTA-San-Andreas\src"
 rm dataset.csv
+ECHO dataset.csv deleted
+
 cd csv_dataset
-rm *
+rm *.jpg
+ECHO Deleted all the images from csv_dataset
 
-cd ..
-cd tf_dataset
+cd ..\tf_dataset
+ECHO into tf_dataset
 
-cd forward
-rm *
-cd ..
+cd forward\
+ECHO into forward
+rm *.jpg
 
-cd backward
-rm *
-cd ..
+cd ..\backward
+ECHO into backward
+rm *.jpg
 
-cd left
-rm *
-cd ..
+cd ..\left
+ECHO into left
+rm *.jpg
 
-cd right
-rm *
-cd ..
+cd ..\right
+ECHO into right
+rm *.jpg
 
-cd forward_left
-rm *
-cd ..
+cd ..\forward_left
+ECHO into forward_left
+rm *.jpg
 
-cd forward_right
-rm *
-cd ..
+cd ..\forward_right
+ECHO into forward right
+rm *.jpg
 
-cd backward_left
-rm *
-cd ..
+cd ..\backward_left
+echo into backward left
+rm *.jpg
 
-cd backward_right
-rm *
+cd ..\backward_right
+echo into backward right
+rm *.jpg
 
-cd "Yogendra D\Self_driving_Car_for_GTA-San-Andreas\src"
-cd other
+cd ..\..\other
+ECHO into other
 python dataset_csv_template.py
+echo dataset.csv created
